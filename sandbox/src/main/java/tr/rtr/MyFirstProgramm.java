2b47b49909f94e3170ecd6ec9df19d0938d5598a
maxon2112 <maxen_93@icloud.com>
2018-11-25 14:27:08 +0300

    Задание 2 Пункт 4

--- sandbox/src/main/java/tr/rtr/MyFirstProgramm.java
+++ sandbox/src/main/java/tr/rtr/MyFirstProgramm.java
@@ -1,16 +1,16 @@
 package tr.rtr;
 
+
+
 public class MyFirstProgramm {
     public static void main(String[] args) {
 
 
+    Point p1 = new Point(5, 5);
+    Point p2 = new Point(10, 10);
 
-    Point p1 = new Point(1, 5);
-    Point p2 = new Point(12, 34);
-    double d = p1.distance(p2);
+        System.out.println("Расстояние между двумя точками p1("+p1.x +","+ p1.y+") и p2("+p2.x +","+ p2.y+")="+p1.distance(p2));
 
-
-    System.out.println(d);
 }
 }
 
