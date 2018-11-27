1c6b6e4d5840cb4ff12d94227a3de9e03dca29ea
maxon2112 <maxen_93@icloud.com>
2018-11-27 21:09:03 +0300

    перенос кусок кода в метод инизалицации текстуры

--- addressbook-web-tests/src/test/java/adressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/adressbook/GroupCreationTests.java
@@ -21,10 +21,6 @@ public class GroupCreationTests {
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
     wd.get("http://localhost/addressbook/edit.php");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("pass")).clear();
@@ -32,6 +28,11 @@ public class GroupCreationTests {
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys("admin");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
+
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
