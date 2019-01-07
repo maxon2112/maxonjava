1fa9793d0fed78310b76145a9216f5bbfe195f7f
maxon2112 <45143158+maxon2112@users.noreply.github.com>
2019-01-07 16:21:51 +0300

    Анализ опций командой строки при запуске генератора данных

--- addressbook-web-tests/src/test/java/adressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/adressbook/generators/GroupDataGenerator.java
@@ -1,6 +1,9 @@
 package adressbook.generators;
 
 import adressbook.model.GroupData;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -8,23 +11,38 @@ import java.util.List;
 
 public class GroupDataGenerator {
 
+    @Parameter(names = "-c", description ="Group count" )
+    public int count;
+
+    @Parameter(names = "-f", description = "Target file")
+    public String file;
+
     public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
-
-        List<GroupData> groups = generateGroups(count);
-        save(groups, file);
-    }
-
-    private static void save(List<GroupData> groups, File file) throws IOException {
-        Writer writer = new FileWriter(file);
-        for (GroupData group : groups) {
-            writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
+        GroupDataGenerator generator = new GroupDataGenerator();
+        JCommander jCommander =new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        }catch (ParameterException ex) {
+            jCommander.usage();
+            return;
         }
-        writer.close();
-    }
+        generator.run();
 
-        private static List<GroupData> generateGroups ( int count){
+    }
+        private void run () throws IOException {
+            List<GroupData> groups = generateGroups(count);
+            save(groups, new File(file));
+        }
+
+        private void save (List < GroupData > groups, File file) throws IOException {
+            Writer writer = new FileWriter(file);
+            for (GroupData group : groups) {
+                writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
+            }
+            writer.close();
+        }
+
+        private List<GroupData> generateGroups ( int count){
             List<GroupData> groups = new ArrayList<GroupData>();
             for (int i = 0; i < count; i++) {
                 groups.add(new GroupData().withName(String.format("test %s", i)).withHeader(String.format("header %s", i)).withFooter(String.format("footer %s", i)));
