2aec15579e4aac07fc52bc90975e717ec69dce58
maxon2112 <45143158+maxon2112@users.noreply.github.com>
2019-01-13 23:19:36 +0300

    Отключаемые проверки UI

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -9,9 +9,16 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import ru.stqa.pft.addressbook.appmanager.dBHelper;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestBase {
     Logger logger = LoggerFactory.getLogger(TestBase.class);
@@ -28,11 +35,11 @@ public class TestBase {
     @AfterSuite(alwaysRun = true)
 
     public void tearDown() throws Exception {
-       app.stop();
+        app.stop();
     }
 
     @BeforeMethod
-    public void logTestStart(Method m, Object[] p){
+    public void logTestStart(Method m, Object[] p) {
         logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
     }
 
@@ -41,4 +48,14 @@ public class TestBase {
         logger.info("Stop test " + m.getName());
     }
 
+    public void verifyGroupListInUI() {
+        if (Boolean.getBoolean("verifyUI")) {
+            Groups dbGroups = app.db().groups();
+            Groups uiGroups = app.group().all();
+            assertThat(uiGroups, equalTo(dbGroups.stream().map((g) -> new GroupData().withId(g.getId()).withName(g.getName())).collect(Collectors.toSet())));
+
+
+        }
+
+    }
 }
